60c7c8f82c1545da820e71358907e5b95639419e
pbarry129 <pbarry@wit.ie>
2016-12-05 01:27:09 +0000

    Add files via upload

---
+++ Plot2.R
@@ -0,0 +1,34 @@
+#Read in the power consumption data
+
+my_power_data<-read.csv("household_power_consumption.txt", sep=";")
+
+# Create date string
+my_power_dates<-paste(as(my_power_data$Date, "character"), as(my_power_data$Time, "character"))
+
+# Put date string in Date format
+POS_dates<-strptime(my_power_dates, "%d/%m/%Y %T", tz="")
+
+# Add Dates to data
+
+power_data <- cbind(my_power_data, POS_dates)
+
+# Get start and end dates
+
+start_date <-strptime("31/01/2007 23:59:59","%d/%m/%Y %T", tz="")
+end_date <- strptime("02/02/2007 23:59:59","%d/%m/%Y %T", tz="")
+
+# Subset the data 
+two_days <- subset(power_data, power_data$POS_dates > start_date & power_data$POS_dates<end_date)
+
+# Plot the data 
+
+plot(two_days$POS_dates,as.numeric(two_days$Global_active_power),type="l", xlab="Time", ylab="Global Active Power (kilowatt)")
+dev.copy(png, file="plot2.png", width=480, height=480, units="px")
+dev.off()
+
+
+
+
+
+
+
